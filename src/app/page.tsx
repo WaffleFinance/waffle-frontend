0331ab4c46afe88b5c4012e64b7d0b87076abd42
Francisco Cordeiro <francisco.cordeiro.business@gmail.com>
2024-07-14 01:00:42 +0200

    improve homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,13 @@ export default function HomePage() {
     <main className="flex h-full w-full flex-col items-start justify-center bg-cover bg-no-repeat">
       <div className="relative flex h-full w-full max-w-desktop items-center justify-start">
         <Image src={kvBrand} alt="Brand image" className="px-16" />
-        <Image src={iphone} alt="phone image" className="absolute bottom-0 right-0 h-auto" width={1200} height={1800} />
+        <Image
+          src={iphone}
+          alt="phone image"
+          className="absolute bottom-0 right-0 h-auto w-[20rem] md:w-[35rem] lg:w-[50rem]"
+          width={800}
+          height={0}
+        />
       </div>
     </main>
   )
