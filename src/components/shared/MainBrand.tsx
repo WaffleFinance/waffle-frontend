0ae91b0a2bc541e45cdd3e08f4239a494990a826
Francisco Cordeiro <francisco.cordeiro.business@gmail.com>
2024-07-13 20:16:39 +0200

    remove pancakes assets

--- src/components/shared/MainBrand.tsx
+++ src/components/shared/MainBrand.tsx
@@ -1,11 +1,3 @@
-import Image from 'next/image'
-import pancake from '@/../public/cta-pancake.webp'
-
 export default function MainBrand() {
-  return (
-    <div className="relative">
-      <p className="text-2xl font-extrabold text-primary lg:text-4xl">NEXT.</p>
-      <Image src={pancake} alt="pancake" className="absolute -left-3 -top-2 size-8 -rotate-[20deg]" />
-    </div>
-  )
+  return <p className="text-2xl font-extrabold text-primary lg:text-4xl">NEXT.</p>
 }
--- src/utils/tokens.tsx
+++ src/utils/tokens.tsx
@@ -1,11 +1,11 @@
 import { zeroAddress } from 'viem'
-import { sepolia, bscTestnet } from 'viem/chains'
+import { flareTestnet, scrollSepolia } from 'viem/chains'
 import { SUPPORTED_TOKENS, Token } from './constants'
 
-import Image from 'next/image'
-import ethLogo from '@/../public/logos/ethereum.svg'
 import btcLogo from '@/../public/logos/btc.svg'
+import ethLogo from '@/../public/logos/ethereum.svg'
 import usdcLogo from '@/../public/logos/usdc.svg'
+import Image from 'next/image'
 
 export const TOKENS: Map<SUPPORTED_TOKENS, Token> = new Map([
   [
@@ -13,7 +13,7 @@ export const TOKENS: Map<SUPPORTED_TOKENS, Token> = new Map([
     {
       id: 'BTC',
       label: 'BTC',
-      address: { [sepolia.id]: '0x1e45f105146d7499fe056d646e55f93dc0dc751f', [bscTestnet.id]: zeroAddress },
+      address: { [scrollSepolia.id]: '0x1e45f105146d7499fe056d646e55f93dc0dc751f', [flareTestnet.id]: zeroAddress },
       icon: <Image src={btcLogo} alt="btc logo" />,
     },
   ],
@@ -22,7 +22,7 @@ export const TOKENS: Map<SUPPORTED_TOKENS, Token> = new Map([
     {
       id: 'ETH',
       label: 'ETH',
-      address: { [sepolia.id]: '0xA2bFA4Cd0171f124Df6ed94a41D79A81B5Ffb42d', [bscTestnet.id]: zeroAddress },
+      address: { [scrollSepolia.id]: '0xA2bFA4Cd0171f124Df6ed94a41D79A81B5Ffb42d', [flareTestnet.id]: zeroAddress },
       icon: <Image src={ethLogo} alt="eth logo" />,
     },
   ],
@@ -31,7 +31,7 @@ export const TOKENS: Map<SUPPORTED_TOKENS, Token> = new Map([
     {
       id: 'USDC',
       label: 'USDC',
-      address: { [sepolia.id]: '0x481366775cf607f9fad7d99fa84465ac06f7f2bc', [bscTestnet.id]: zeroAddress },
+      address: { [scrollSepolia.id]: '0x481366775cf607f9fad7d99fa84465ac06f7f2bc', [flareTestnet.id]: zeroAddress },
       icon: <Image src={usdcLogo} alt="usdc logo" />,
     },
   ],
